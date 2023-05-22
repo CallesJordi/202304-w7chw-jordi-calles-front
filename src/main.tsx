eebc1e2d116022fce051a090d3f6eefa630ef497
Jordi Calles <jordicalles1989@gmail.com>
2023-05-22 14:58:49 +0200

    Add Layout test

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./components/App/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import GlobalStyle from "./styles/globlaStyles";
+import GlobalStyle from "./styles/GloblaStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
